462827064a163af7cd60a771a947f6ae1239019f
notluu <lukaschiu@gmail.com>
2023-01-12 17:07:21 +0800

    ah

--- HomePage.js
+++ HomePage.js
@@ -10,10 +10,16 @@ import {
     KeyboardAvoidingView
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from 'react-native-vector-icons/MaterialIcons';
+
 
 function HomePage() {
 
     const navigation = useNavigation();
+    const [searchText, setSearchText] = useState('');
+    const handleSearch = (searchText) => {
+        console.log(`Search text: ${searchText}`);
+    }
 
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -22,13 +28,18 @@ function HomePage() {
                 behavior="padding"
                 keyboardVerticalOffset={60}
             >
-                <View>
+                <View style={styles.TopQ}>
                     <TextInput
                         style={styles.searchBar}
                         placeholder="Search..."
                         onChangeText={text => setSearchText(text)}
                         value={searchText}
                     />
+                    <TouchableOpacity onPress={() => handleSearch(searchText)}>
+                        <Text>Search</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.BottomQ}>
                 </View>
             </KeyboardAvoidingView>
         </TouchableWithoutFeedback>
@@ -40,6 +51,29 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'column',
         justifyContent: 'stretch',
+    },
+    searchBar: {
+        margin: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#676767',
+        borderRadius: 5,
+        backgroundColor: 'white',
+    },
+    TopQ: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#DA5858',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    BottomQ: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#fff',
+    },
+    searchIcon: {
+        padding: 5,
     },
 });
 
